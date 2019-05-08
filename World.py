6dfb0b204f21f54dfb2a60d1f54f69a0e7f91838
Kevin Ge <gekevin1998@gmail.com>
2019-05-08 13:21:57 -0700

    Create World.py

---
+++ World.py
@@ -0,0 +1,63 @@
+SUMMARY = "Combat AI"
+
+def getMissionXML(agent_id):
+    ''' Build an XML mission string '''
+
+    return '''<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
+    <Mission xmlns="http://ProjectMalmo.microsoft.com" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+        <About>
+            <Summary>''' + SUMMARY + '''</Summary>
+            <Description>Test01</Description>
+        </About>
+
+        <ModSettings>
+            <MsPerTick>100</MsPerTick>
+        </ModSettings>
+
+        <ServerSection>
+            <ServerInitialConditions>
+                <Time>
+                    <StartTime>6000</StartTime>
+                    <AllowPassageOfTime>false</AllowPassageOfTime>
+                </Time>
+                <Weather>clear</Weather>
+                <AllowSpawning>false</AllowSpawning>
+            </ServerInitialConditions>
+            <ServerHandlers>
+                <FlatWorldGenerator generatorString="3;7,220*1,5*3,2;3;,biome_1" />
+                <DrawingDecorator>
+                    <DrawCuboid x1="-50" y1="100" z1="-50" x2="50" y2="100" z2="50" type="grass_block" />
+                    <DrawCuboid x1="-51" y1="102" z1="-51" x2="50" y2="102" z2="-51" type="oak_slab" />
+                    <DrawCuboid x1="51" y1="102" z1="-51" x2="51" y2="102" z2="50" type="oak_slab" />
+                    <DrawCuboid x1="51" y1="102" z1="51" x2="-50" y2="102" z2="51" type="oak_slab" />
+                    <DrawCuboid x1="-51" y1="102" z1="51" x2="-51" y2="102" z2="-50" type="oak_slab" />
+                </DrawingDecorator>
+                <ServerQuitWhenAnyAgentFinishes />
+            </ServerHandlers>
+        </ServerSection>
+
+        <AgentSection mode="Survival">
+            <Name>'''Player''' + agent_id</Name>
+            <AgentStart>
+                <Placement x="-25.5" y="100.0" z="0.5"/>
+                <Inventory>
+                    <InventoryItem slot="0" type="stone_sword" />
+                    <InventoryItem slot="1" type="stone_axe" />
+                    <InventoryItem slot="2" type="bow" />
+                    <InventoryItem slot="9" type="arrow" quantity="64" />
+                    <InventoryItem slot="-106" type="shield" />
+                </Inventory>
+            </AgentStart>
+            <AgentHandlers>
+                <ContinuousMovementCommands turnSpeedDegs="480"/>
+                <AbsoluteMovementCommands/>
+                <MissionQuitCommands/>
+                <InventoryCommands/>
+                <ObservationFromHotBar/>
+                <ObservationFromNearbyEntities>
+                    <Range name="entities" xrange="40" yrange="40" zrange="40"/>
+                </ObservationFromNearbyEntities>
+            </AgentHandlers>
+        </AgentSection>
+
+    </Mission>'''
